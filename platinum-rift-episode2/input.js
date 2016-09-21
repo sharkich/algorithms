8beff544a3ceca70f5bbca04b762a869bc91b851
Artem Koziar <a@temich.in.ua>
2016-09-21 10:41:41 +0300

    Platinum Rift - Episode 2: create map.txt

---
+++ platinum-rift-episode2/input.js
@@ -0,0 +1,22 @@
+const INPUTS_GLOBAL = readline();
+printErr(`INPUTS_GLOBAL: ${INPUTS_GLOBAL}`);
+for (let i = 0; i < 94; i++) {
+    const ZONES = readline();
+    printErr(`ZONES: ${ZONES}`);
+}
+for (let i = 0; i < 143; i++) {
+    const LINKS = readline();
+    printErr(`LINKS: ${LINKS}`);
+}
+while (true) {
+    const MY_PLATINUM = parseInt(readline());
+    printErr(`MY_PLATINUM: ${MY_PLATINUM}`);
+
+    for (let i = 0; i < 94; i++) {
+        const INPUTS = readline();
+        printErr(`INPUTS: ${INPUTS}`);
+    }
+    print('WAIT');
+    print('WAIT');
+
+}
